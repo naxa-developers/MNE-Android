77dc1da227b25e893222686b4ba929c4027d5e4a
Nishon Tandukar <nishon.tan@gmail.com>
2019-04-08 11:09:22 +0545

    [#28] changes list ui

--- collect_app/src/main/java/org/odk/collect/android/myapplication/beneficary/BeneficiariesActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/myapplication/beneficary/BeneficiariesActivity.java
@@ -1,10 +1,9 @@
 package org.odk.collect.android.myapplication.beneficary;
 
-import android.arch.lifecycle.Observer;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -17,16 +16,14 @@ import org.odk.collect.android.myapplication.common.view.RecyclerViewEmptySuppor
 import java.util.HashMap;
 import java.util.List;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
 public class BeneficiariesActivity extends BaseActivity {
 
     DisposableObserver<Object> dis;
     private Toolbar toolbar;
-    private RecyclerViewEmptySupport recyclerView;
+    private RecyclerView recyclerView;
     private TitleDescAdapter listAdapter;
     private BaseRecyclerViewAdapter<BeneficaryResponse, BeneficaryVH> adapter;
     private HashMap<String, String> hashMap;
@@ -73,15 +70,6 @@ public class BeneficiariesActivity extends BaseActivity {
         LinearLayoutManager manager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setEmptyView(findViewById(R.id.root_layout_empty_layout), "No data"
-                , new RecyclerViewEmptySupport.OnEmptyLayoutClickListener() {
-                    @Override
-                    public void onRetryButtonClick() {
-
-                    }
-                });
-
-
 
         adapter = new BaseRecyclerViewAdapter<BeneficaryResponse, BeneficaryVH>(beneficaryResponses, R.layout.list_item_beneficary) {
             @Override
