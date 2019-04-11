c0b979b5dc2c203123ccbc5b47f32a31086a5096
Nishon Tandukar <nishon.tan@gmail.com>
2019-04-11 13:31:18 +0545

    [#36] changed date ui

--- collect_app/src/main/java/org/odk/collect/android/myapplication/activity/ActivityVH.java
+++ collect_app/src/main/java/org/odk/collect/android/myapplication/activity/ActivityVH.java
@@ -2,41 +2,39 @@ package org.odk.collect.android.myapplication.activity;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.odk.collect.android.R;
 import org.odk.collect.android.myapplication.activitygroup.model.Activity;
-import org.odk.collect.android.myapplication.beneficary.BeneficaryResponse;
-import org.odk.collect.android.myapplication.beneficary.BeneficiariesActivity;
-import org.odk.collect.android.myapplication.utils.ActivityUtil;
-
-import java.util.HashMap;
+import org.odk.collect.android.myapplication.common.DateUtils;
 
 public class ActivityVH extends RecyclerView.ViewHolder {
 
     private TextView tvTitle, tvDesc, tvOutput, tvIconText;
-    private TextView tvTargetNumber, tvStartDate, tvEndDate, tvTargetUnit;
+    private TextView tvTargetNumber, tvDate, tvEndDate, tvTargetUnit;
 
     ActivityVH(View itemView) {
         super(itemView);
         tvTitle = itemView.findViewById(R.id.primary_text);
         tvDesc = itemView.findViewById(R.id.sub_text);
         tvTargetNumber = itemView.findViewById(R.id.target);
-        tvStartDate = itemView.findViewById(R.id.start_date);
-        tvEndDate = itemView.findViewById(R.id.end_date);
+        tvDate = itemView.findViewById(R.id.tv_date);
+
         tvTargetUnit = itemView.findViewById(R.id.target_unit);
     }
 
     public void bindView(Activity desc) {
         tvTitle.setText(desc.getName());
         tvDesc.setText(desc.getDescription());
-        tvStartDate.setText(desc.getStartDate());
-        tvEndDate.setText(desc.getEndDate());
+
+        String formatedDate = "From" + " " + DateUtils.formatDate(desc.getStartDate()) + " " + "to" + " " + DateUtils.formatDate(desc.getEndDate());
+        tvDate.setText(formatedDate);
         String formattedUnit = desc.getTargetNumber() + " " + desc.getTargetUnit();
+
+
         tvTargetNumber.setText(formattedUnit);
         itemView.setOnClickListener(v -> {
-           viewItemClicked(desc);
+            viewItemClicked(desc);
         });
     }
 
--- collect_app/src/main/java/org/odk/collect/android/myapplication/common/DateUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/myapplication/common/DateUtils.java
@@ -3,10 +3,28 @@ package org.odk.collect.android.myapplication.common;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
+import java.util.concurrent.TimeUnit;
+
+import timber.log.Timber;
+
 public class DateUtils {
 
     public static boolean hasEndDatePassed(String endate) {
-        DateTime dateTime = DateTime.parse(endate, DateTimeFormat.forPattern("yyyy-mm-dd"));
+        DateTime dateTime = DateTime.parse(endate, DateTimeFormat.forPattern("yyyy-MM-dd"));
         return dateTime.isBeforeNow();
     }
+
+
+    public static String formatDate(String endate) {
+        try {
+            DateTime dateTime = DateTime.parse(endate, DateTimeFormat.forPattern("yyyy-MM-dd"));
+            String formattedDate = dateTime.toString(DateTimeFormat.longDate());
+            Timber.i("Formatted %s to %s", endate, formattedDate);
+            return formattedDate;
+        } catch (IllegalArgumentException e) {
+            Timber.e(e);
+            return endate;
+        }
+    }
+
 }
